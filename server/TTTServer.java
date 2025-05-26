b8e7d7d55951a02482c8880a45a24b0e25056859
Suyoung Seo <102464954+cprypt@users.noreply.github.com>
2025-05-27 02:07:48 +0900

    Update TTTServer.java

--- server/TTTServer.java
+++ server/TTTServer.java
@@ -36,7 +36,7 @@ public class TTTServer {
             // 1) 보드 상태를 모든 클라이언트에게 전송
             sm.broadcast(mh.formatBoard(gl.getBoard()));
             // 2) 현재 플레이어에게 움직임 요청
-            sm.broadcast(MessageHandler.MOVE);
+            sm.sendTo(currentPlayer, MessageHandler.MOVE);
 
             // 3) 클라이언트로부터 MOVE 메시지 수신
             String moveMsg = sm.receiveFrom(currentPlayer);
